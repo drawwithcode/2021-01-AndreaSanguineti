287d132787ed09415139b8d519b032a0982442c1
AndreaSanguineti <andrealaura.sanguineti@mail.polimi.it>
2021-09-28 23:27:23 +0200

    Sanguineti_Assignment1

--- sketch.js
+++ sketch.js
@@ -1,13 +1,43 @@
+var song;
+
 function preload() {
-  // put preload code here
+  song = loadSound("youth.mp3");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  angleMode(DEGREES);
+  background(220);
+  background("red");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noStroke;
+  fill("#FFFFFF");
+  textAlign(CENTER);
+  textSize(28);
+  text("click for sound on / off", width / 2, height / 2);
+
+  noFill();
+
+  let changeColor = lerpColor(color("red"), color("blue"), frameCount / 150);
+
+  stroke(changeColor);
+  translate(frameCount * 2, height / 2);
+  circle(0, 0, 1000);
+
+  translate(-frameCount * 4, 0);
+  circle(width, 0, 1000);
+
+  if (frameCount === 1000) {
+    noLoop();
+  }
+}
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
 }
