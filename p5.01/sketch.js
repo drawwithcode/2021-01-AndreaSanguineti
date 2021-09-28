1be9be606d07566f3f0d9f82df185d852b9734ea
AndreaSanguineti <90753738+AndreaSanguineti@users.noreply.github.com>
2021-09-29 00:06:58 +0200

    Add files via upload

---
+++ p5.01/sketch.js
@@ -0,0 +1,43 @@
+var song;
+
+function preload() {
+  song = loadSound("youth.mp3");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  background(220);
+  background("red");
+}
+
+function draw() {
+  noStroke;
+  fill("#FFFFFF");
+  textAlign(CENTER);
+  textSize(28);
+  text("click for sound on / off", width / 2, height / 2);
+
+  noFill();
+
+  let changeColor = lerpColor(color("red"), color("blue"), frameCount / 150);
+
+  stroke(changeColor);
+  translate(frameCount * 2, height / 2);
+  circle(0, 0, 1000);
+
+  translate(-frameCount * 4, 0);
+  circle(width, 0, 1000);
+
+  if (frameCount === 800) {
+    noLoop();
+  }
+}
+
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
